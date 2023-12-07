35e08d068056f5c23d1881c31e4db8029b59f7d0
Ayush Kumar <ak487457@gmail.com>
2023-12-07 14:22:20 +0530

    Setting environment variables

--- src/main/java/com/ayushtech/flagbot/Main.java
+++ src/main/java/com/ayushtech/flagbot/Main.java
@@ -1,8 +1,5 @@
 package com.ayushtech.flagbot;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
 import javax.security.auth.login.LoginException;
 
 import com.ayushtech.flagbot.dbconnectivity.DBInfo;
@@ -22,17 +19,22 @@ public class Main
     
     public static void main( String[] args ) throws LoginException, InterruptedException
     {
-        Properties properties = new Properties();
-        try {
-            properties.load(new FileInputStream("credential.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Properties properties = new Properties();
+        // try {
+        //     properties.load(new FileInputStream("credential.properties"));
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
         
-        final String bot_token = properties.getProperty("BOT_TOKEN");
-        final String db_host = properties.getProperty("database_url");
-        final String db_username = properties.getProperty("database_username");
-        final String db_password = properties.getProperty("database_password");
+        // final String bot_token = properties.getProperty("BOT_TOKEN");
+        // final String db_host = properties.getProperty("database_url");
+        // final String db_username = properties.getProperty("database_username");
+        // final String db_password = properties.getProperty("database_password");
+        final String bot_token = System.getenv("bot_token");
+        final String db_host = System.getenv("db_url");
+        final String db_username = System.getenv("db_username");
+        final String db_password = System.getenv("db_password");
+
         DBInfo.setData(db_host, db_username, db_password);
         
         JDA jda = JDABuilder.createDefault(bot_token).addEventListeners(new MessageListener(), new InteractionsListener())
